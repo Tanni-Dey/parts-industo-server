5ff56ac9b9d3e5090ae8a443b89386f5f883226e
Tanni-Dey <tonnidey277615@gmail.com>
2022-05-22 16:37:14 +0600

    initial-setup

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const jwt = require('jsonwebtoken')
 const app = express();
@@ -8,6 +9,16 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(express.json())
 app.use(cors())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.stdsb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
 
 app.get('/', (req, res) => {
     res.send('parts-industo')
