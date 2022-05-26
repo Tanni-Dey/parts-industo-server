68b2a8cfa7a2bf9f1af1b2e3fda1e9d8befdd002
Tanni-Dey <tonnidey277615@gmail.com>
2022-05-26 11:30:07 +0600

    adminapi

--- index.js
+++ index.js
@@ -13,6 +13,23 @@ app.use(cors())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.stdsb.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJwt(req, res, next) {
+    const autheader = req.headers.authorization;
+    if (!autheader) {
+        return res.status(401).send({ message: 'Unauthorized Access' })
+    }
+    const token = autheader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbiden Access' })
+        }
+        req.decoded = decoded
+        next()
+
+    })
+
+}
 async function run() {
     try {
         await client.connect();
@@ -35,6 +52,15 @@ async function run() {
             res.send(allReviews)
         })
 
+
+        //load all users api
+        app.get('/user', verifyJwt, async (req, res) => {
+            const query = {}
+            const allUsers = await usersCollection.find(query).toArray()
+            res.send(allUsers)
+        })
+
+
         //update or insert all user api
         app.put('/user/:email', async (req, res) => {
             const userEmail = req.params.email;
@@ -45,8 +71,38 @@ async function run() {
                 $set: user
             }
             const result = await usersCollection.updateOne(filter, udateUser, options)
-            res.send(result)
+            const token = jwt.sign({ email: userEmail }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+            res.send({ result, token })
         })
+
+        //admin check
+        app.get('/admin/:email', verifyJwt, async (req, res) => {
+            const email = req.params.email;
+            const user = await usersCollection.findOne({ email: email })
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
+
+        //make admin api
+        app.put('/user/admin/:email', verifyJwt, async (req, res) => {
+            const userEmail = req.params.email;
+            // const user = req.body;
+            const filter = { email: userEmail }
+            const decodedEmail = req.decoded.email
+            const user = await usersCollection.findOne({ email: decodedEmail })
+            if (user.role === 'admin') {
+                const udateUser = {
+                    $set: { role: 'admin' }
+                }
+                const result = await usersCollection.updateOne(filter, udateUser)
+                res.send(result)
+            }
+            else {
+                return res.status(403).send({ message: 'Forbiden Access' })
+
+            }
+        })
+
 
         //load signle tool api
         app.get('/tool/:id', async (req, res) => {
@@ -71,11 +127,18 @@ async function run() {
         })
 
         //get my orders api with email query
-        app.get('/myorder', async (req, res) => {
+        app.get('/myorder', verifyJwt, async (req, res) => {
             const userEmail = req.query.email;
+            const requester = req.decoded?.email;
             const query = { email: userEmail }
-            const userOrders = await ordersCollection.find(query).toArray()
-            res.send(userOrders)
+            if (userEmail === requester) {
+                const userOrders = await ordersCollection.find(query).toArray()
+                return res.send(userOrders)
+            }
+            else {
+                return res.status(403).send({ message: 'Forbiden access' })
+            }
+
         })
 
 
